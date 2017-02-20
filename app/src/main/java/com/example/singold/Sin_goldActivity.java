ef74dbb007605c3264917e4270582506f2148fd9
aleen <alona.nassra@hotmail.com>
2017-02-20 13:54:38 -0800

    aleen

---
+++ app/src/main/java/com/example/singold/Sin_goldActivity.java
@@ -0,0 +1,13 @@
+package com.example.singold;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sin_goldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sin_gold);
+    }
+}
