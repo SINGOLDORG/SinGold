873e0682c1d67a0da4d11913be7852d10d9553a8
aleen <alona.nassra@hotmail.com>
2017-03-01 07:56:24 -0800

    finishing java

--- app/src/main/java/com/example/singold/startActivity.java
+++ app/src/main/java/com/example/singold/startActivity.java
@@ -2,12 +2,16 @@ package com.example.singold;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class startActivity extends AppCompatActivity {
+public class startActivity extends AppCompatActivity
+{
+    private Button back3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        back3=(Button)findViewById(R.id.back3);
     }
 }
