49e86c674f52f1a25d6bf8c759f95b5996a189fa
user <rosette.draz@outlook.co.il>
2017-03-03 06:40:38 -0800

    halfSurvey

--- app/src/main/java/com/example/singold/data/PationtSurvey.java
+++ app/src/main/java/com/example/singold/data/PationtSurvey.java
@@ -9,6 +9,10 @@ public class PationtSurvey {
     private String id;
     @com.google.gson.annotations.SerializedName("country")
     private String country;
+    @com.google.gson.annotations.SerializedName("city")
+    private String city;
+    @com.google.gson.annotations.SerializedName("year")
+    private String year;
     @com.google.gson.annotations.SerializedName("homeMusic")
     private String homeMusic;
     @com.google.gson.annotations.SerializedName("education")
@@ -17,7 +21,7 @@ public class PationtSurvey {
     private String isMahazot;
     @com.google.gson.annotations.SerializedName("mahazot")
     private String mahazot;
-
+//hkltot,disk,..
     @com.google.gson.annotations.SerializedName("fSinger")
     private String fSinger;
     @com.google.gson.annotations.SerializedName("fSongs")
@@ -56,6 +60,14 @@ public class PationtSurvey {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
     }
 
     public String getHomeMusic() {
