e93d28b5360f17bedfdf0fbeaeca62cca65c5b6e
user <rosette.draz@outlook.co.il>
2017-03-13 14:44:41 -0700

    MatchingSurvey//

--- app/src/main/java/com/example/singold/data/ConnectToServer.java
+++ app/src/main/java/com/example/singold/data/ConnectToServer.java
@@ -44,7 +44,7 @@ public  class ConnectToServer {
     private static MobileServiceTable<Song> songTable;
     private static MobileServiceTable<PatientDetails> patientDetailsTable;
     private static MobileServiceTable<PatientSurvey> patientSurveyTable;
-    private static MobileServiceTable<HalfSurvey> HalfSurveyTable;
+    private static MobileServiceTable<MatchingSurvey> MatchingSurveyTable;
     private static MobileServiceTable<Login> loginTable;
 
 
@@ -455,15 +455,15 @@ public  class ConnectToServer {
 
         runAsyncTask(task);
     }
-    public static void addInTable(final HalfSurvey item) throws ExecutionException, InterruptedException {
+    public static void addInTable(final MatchingSurvey item) throws ExecutionException, InterruptedException {
         ;
-        if (HalfSurveyTable == null)
-            HalfSurveyTable = mClient.getTable(HalfSurvey.class);
+        if (MatchingSurveyTable == null)
+            MatchingSurveyTable = mClient.getTable(MatchingSurvey.class);
         AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
             @Override
             protected Void doInBackground(Void... params) {
                 try {
-                    final HalfSurvey entity = HalfSurveyTable.insert(item).get();
+                    final MatchingSurvey entity = MatchingSurveyTable.insert(item).get();
 
                     context.runOnUiThread(new Runnable() {
                         @Override
