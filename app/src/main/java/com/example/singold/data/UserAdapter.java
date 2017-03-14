8383d9537d39d104dcd773abf9081b46d1536d5c
Ayah <ayah_aslan@outlook.com>
2017-03-14 07:44:14 -0700

    Ayah

---
+++ app/src/main/java/com/example/singold/data/UserAdapter.java
@@ -0,0 +1,32 @@
+package com.example.singold.data;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.google.gson.internal.bind.ArrayTypeAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 14/03/2017.
+ */
+
+public class UserAdapter extends ArrayAdapter<User>
+{
+
+    public UserAdapter(Context context, int resource)
+    {
+        super(context, resource);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final User User = getItem(position);
+    }
+
+
+}
+
+
