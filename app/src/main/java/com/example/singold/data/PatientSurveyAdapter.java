04b74668f14f6fa24f2aad3de81d9a2a1fa7d987
Ayah <ayah_aslan@outlook.com>
2017-03-17 09:09:20 -0700

    Mission is finished

--- app/src/main/java/com/example/singold/data/PatientSurveyAdapter.java
+++ app/src/main/java/com/example/singold/data/PatientSurveyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.singold.R;
 
@@ -12,20 +13,59 @@ import com.example.singold.R;
  * Created by user on 16/03/2017.
  */
 
-public class PatientSurveyAdapter extends ArrayAdapter<PatientSurvey>
-{
-    public PatientSurveyAdapter (Context context, int resource) {
+public class PatientSurveyAdapter extends ArrayAdapter<PatientSurvey> {
+    public PatientSurveyAdapter(Context context, int resource) {
         super(context, resource);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         final PatientSurvey PatientSurvey = getItem(position);
 
-        if(convertView==null)
-        {
-            convertView= LayoutInflater.from(getContext()).inflate(R.layout.PatientSurveyitem,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_patient_survey, parent, false);
         }
+
+        TextView textId = (TextView) convertView.findViewById(R.id.textId);
+        TextView textIdPatient = (TextView) convertView.findViewById(R.id.textIdPatient);
+        TextView textCountry = (TextView) convertView.findViewById(R.id.textCountry);
+        TextView textCity = (TextView) convertView.findViewById(R.id.textCity);
+        TextView textYear = (TextView) convertView.findViewById(R.id.textYear);
+        TextView textHomeMusic = (TextView) convertView.findViewById(R.id.textHomeMusic);
+        TextView textEducation = (TextView) convertView.findViewById(R.id.textEducation);
+        TextView textIsMahazot = (TextView) convertView.findViewById(R.id.textIsMahazot);
+        TextView textMahazot = (TextView) convertView.findViewById(R.id.textMahazot);
+        TextView textFsinger = (TextView) convertView.findViewById(R.id.textFsinger);
+        TextView textFsongs = (TextView) convertView.findViewById(R.id.textFSongs);
+        TextView textWedding = (TextView) convertView.findViewById(R.id.textWedding);
+        TextView textClassic = (TextView) convertView.findViewById(R.id.textClassic);
+        TextView textIsrael = (TextView) convertView.findViewById(R.id.textIsrael);
+        TextView textLanguage = (TextView) convertView.findViewById(R.id.textLanguage);
+        TextView textDance = (TextView) convertView.findViewById(R.id.textDance);
+        TextView textLoazi = (TextView) convertView.findViewById(R.id.textLoazi);
+        TextView textReligion = (TextView) convertView.findViewById(R.id.textReligion);
+        TextView textRelaxing = (TextView) convertView.findViewById(R.id.textRelaxing);
+
+        textId.setText(PatientSurvey.getId());
+        textIdPatient.setText(PatientSurvey.getIdPatient());
+        textCountry.setText(PatientSurvey.getCountry());
+        textCity.setText(PatientSurvey.getCity());
+        textYear.setText(PatientSurvey.getYear());
+        textHomeMusic.setText(PatientSurvey.getHomeMusic());
+        textEducation.setText(PatientSurvey.getEducation());
+        textIsMahazot.setText(PatientSurvey.getIsMahazot());
+        textMahazot.setText(PatientSurvey.getMahazot());
+        textFsinger.setText(PatientSurvey.getfSinger());
+        textFsongs.setText(PatientSurvey.getfSongs());
+        textWedding.setText(PatientSurvey.getWedding());
+        textClassic.setText(PatientSurvey.getClassic());
+        textIsrael.setText(PatientSurvey.getIsrael());
+        textLanguage.setText(PatientSurvey.getLanguage());
+        textDance.setText(PatientSurvey.getDance());
+        textLoazi.setText(PatientSurvey.getLoazi());
+        textReligion.setText(PatientSurvey.getReligion());
+        textRelaxing.setText(PatientSurvey.getRelaxing());
+
+        return convertView;
     }
 }
--- app/src/main/java/com/example/singold/data/UserAdapter.java
+++ app/src/main/java/com/example/singold/data/UserAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.singold.R;
 import com.google.gson.internal.bind.ArrayTypeAdapter;
@@ -26,10 +27,39 @@ public class UserAdapter extends ArrayAdapter<User> {
     public View getView(int position, View convertView, ViewGroup parent) {
         final User User = getItem(position);
 
-        if(convertView==null)
-        {
-            convertView= LayoutInflater.from(getContext()).inflate(R.layout.Useritem,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_user, parent, false);
         }
+
+        TextView textId = (TextView) convertView.findViewById(R.id.textId);
+        TextView textIdInstitute = (TextView) convertView.findViewById(R.id.textidInstitute);
+        TextView textFN = (TextView) convertView.findViewById(R.id.textFN);
+        TextView textLN = (TextView) convertView.findViewById(R.id.textLN);
+        TextView textUsername = (TextView) convertView.findViewById(R.id.textUsername);
+        TextView textEnterId = (TextView) convertView.findViewById(R.id.textEnterId);
+        TextView textConfirmId = (TextView) convertView.findViewById(R.id.textConfirmId);
+
+        textId.setText(User.getId());
+        textIdInstitute.setText(User.getIdInstitute());
+        textFN.setText(User.getfName());
+        textLN.setText(User.getlName());
+        textUsername.setText(User.getUsername());
+        textEnterId.setText(User.getEnterId());
+        textConfirmId.setText(User.getConfId());
+
+        return convertView;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
