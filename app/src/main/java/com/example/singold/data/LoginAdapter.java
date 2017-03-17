f375872e1855c6e200c41979ac2edb1f88c739e1
Ayah <ayah_aslan@outlook.com>
2017-03-17 08:13:27 -0700

    Ayah

--- app/src/main/java/com/example/singold/data/LoginAdapter.java
+++ app/src/main/java/com/example/singold/data/LoginAdapter.java
@@ -5,6 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.singold.R;
 
@@ -17,12 +20,23 @@ public class LoginAdapter extends ArrayAdapter<Login>
     public LoginAdapter(Context context, int resource) {
         super(context, resource);
     }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
         final Login Login = getItem(position);
 
-        if(convertView==null)
+        if (convertView == null)
         {
-            convertView= LayoutInflater.from(getContext()).inflate(R.layout.item_login,parent,false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_login, parent, false);
         }
+
+        TextView textId = (TextView) convertView.findViewById(R.id.textId);
+        TextView textUserName = (TextView) convertView.findViewById(R.id.textUserName);
+
+        textId.setText(Login.getId());
+        textUserName.setText(Login.getUsername());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/singold/data/MatchingSurveyAdapter.java
+++ app/src/main/java/com/example/singold/data/MatchingSurveyAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.singold.R;
 
@@ -18,12 +19,30 @@ public class MatchingSurveyAdapter extends ArrayAdapter<MatchingSurvey> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         final MatchingSurvey MatchingSurvey = getItem(position);
 
-        if(convertView==null)
-        {
-            convertView= LayoutInflater.from(getContext()).inflate(R.layout.MatchingSurveyitem,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_matching_survey, parent, false);
         }
+
+        TextView textId = (TextView) convertView.findViewById(R.id.textId);
+        TextView textCountry = (TextView) convertView.findViewById(R.id.textCountry);
+        TextView textYear = (TextView) convertView.findViewById(R.id.textYear);
+        TextView textLanguage = (TextView) convertView.findViewById(R.id.textLanguage);
+        TextView textCity = (TextView) convertView.findViewById(R.id.textCity);
+
+        textId.setText(MatchingSurvey.getId());
+        textCountry.setText(MatchingSurvey.getCountry());
+        textYear.setText(MatchingSurvey.getYear());
+        textLanguage.setText(MatchingSurvey.getLanguage());
+        textCity.setText(MatchingSurvey.getCity());
+
+        return convertView;
+    }
 }
+
+
+
+
+
