ef50be5a175985685d8674753b17fbd086b719eb
user <rosette.draz@outlook.co.il>
2017-03-20 05:48:50 -0700

    data handler

--- app/src/main/java/com/example/singold/LogInActivity.java
+++ app/src/main/java/com/example/singold/LogInActivity.java
@@ -45,17 +45,7 @@ public class LogInActivity extends AppCompatActivity
         }
         if (isok==true)
         {
-            Login login=new Login();
-            login.setUsername(stUsername);
-            login.setId(stId);
-            try {
-                ConnectToServer.connet(this);
-                ConnectToServer.addInTable(login);
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
 
         }
 }
