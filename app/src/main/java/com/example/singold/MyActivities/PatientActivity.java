747476b80e735b158feb1cb1fcd7460e9f3f27f1
user <rosette.draz@outlook.co.il>
2017-04-01 02:54:43 -0700

    Rosette..

--- app/src/main/java/com/example/singold/MyActivities/PatientActivity.java
+++ app/src/main/java/com/example/singold/MyActivities/PatientActivity.java
@@ -5,14 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.singold.R;
 import com.example.singold.data.MatchingSurvey;
+import com.example.singold.data.PatientDetails;
 
 public class PatientActivity extends AppCompatActivity implements View.OnClickListener
 {
     private Button playList;
     private Button newMatching;
+    private PatientDetails patientDetails;
+    private TextView tvname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,8 +26,16 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
 
         playList = (Button) findViewById(R.id.playList);
         newMatching = (Button) findViewById(R.id.newMatching);
+        tvname= (TextView) findViewById(R.id.tvname);
         newMatching.setOnClickListener(this);
         playList.setOnClickListener(this);
+        Intent i=getIntent();
+        if(i!=null)
+        {
+            patientDetails = (PatientDetails) i.getExtras().get("patient");
+            tvname.setText(patientDetails.getfName());
+
+        }
     }
     public void onClick(View v)
     {
