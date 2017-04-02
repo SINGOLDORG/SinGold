a492c36c234de066be4bd6d0ccdf951e24a059a9
aleen <alona.nassra@hotmail.com>
2017-04-02 01:53:41 -0700

    Aleen

---
+++ app/src/main/java/com/example/singold/data/FoundSongAdapter.java
@@ -0,0 +1,46 @@
+package com.example.singold.data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.singold.R;
+
+/**
+ * Created by user on 16/03/2017.
+ */
+
+public class FoundSongAdapter extends ArrayAdapter<Song> {
+    public FoundSongAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Song Song = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_found_song, parent, false);
+        }
+
+        CheckBox checkBox=(CheckBox)convertView.findViewById(R.id.checkBox);
+        TextView textName = (TextView) convertView.findViewById(R.id.textName);
+        TextView textSinger = (TextView) convertView.findViewById(R.id.textSinger);
+        TextView textLink = (TextView) convertView.findViewById(R.id.textLink);
+
+
+        textName.setText(Song.getName());
+        textSinger.setText(Song.getSinger());
+        textLink.setText(Song.getLink());
+
+        return convertView;
+    }
+}
+
+
+
