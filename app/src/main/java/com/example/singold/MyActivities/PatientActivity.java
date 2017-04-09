0ce33aecc8ef4eb17dd153c660d4ddc9234ff807
user <rosette.draz@outlook.co.il>
2017-04-09 12:50:09 -0700

    Rosette..

--- app/src/main/java/com/example/singold/MyActivities/PatientActivity.java
+++ app/src/main/java/com/example/singold/MyActivities/PatientActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -83,6 +84,7 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
     private RadioButton chazanout;
     private RadioButton fromPray;
     private RadioButton propnents;
+    private PatientProfile patientProfile;
 
 
     private PatientDetailsAdapter detailsAdapter;
@@ -91,7 +93,13 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
     private ImageButton btnMatch;
     private ImageButton btnMusic;
     private ImageButton btnQuest;
-
+    private EditText textClassic;
+    private EditText textIsrael;
+    private EditText language;
+    private EditText textRelaxing;
+    private EditText textReligion;
+    private EditText textLoazi;
+    private EditText textDance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -105,6 +113,19 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
         year=(EditText)findViewById(R.id.year);
         familyPhone=(EditText)findViewById(R.id.familyPhone);
         pId=(EditText)findViewById(R.id.pId);
+        homemusic=(EditText)findViewById(R.id.homemusic);
+        youngmusic=(EditText)findViewById(R.id.youngmusic);
+        favoritesinger=(EditText)findViewById(R.id.favoritesinger);
+        favoritesong=(EditText)findViewById(R.id.favoritesong);
+        textClassic=(EditText) findViewById(R.id.textClassic);
+        textIsrael=(EditText) findViewById(R.id.textIsrael);
+        language=(EditText)findViewById(R.id.language);
+        textLoazi=(EditText)findViewById(R.id.textLoazi);
+        textRelaxing=(EditText) findViewById(R.id.textRelaxing);
+        textReligion=(EditText)findViewById(R.id.textReligion);
+        textDance=(EditText)findViewById(R.id.textDance);
+
+
         save=(Button)findViewById(R.id.save);
 
 
@@ -123,6 +144,18 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
             address.setText(patientDetails.getAddress());
             year.setText(patientDetails.getYear());
 
+            patientProfile=(PatientProfile) i.getExtras().get("profile") ;
+            homemusic.setText(patientProfile.getHomeMusic());
+            youngmusic.setText(patientProfile.getYoungMusic());
+            favoritesinger.setText(patientProfile.getfSinger());
+            favoritesong.setText(patientProfile.getfSongs());
+            textClassic.setText(patientProfile.getClassic());
+            textIsrael.setText(patientProfile.getIsrael());
+            language.setText(patientProfile.getLanguage());
+            textLoazi.setText(patientProfile.getLoazi());
+            textRelaxing.setText(patientProfile.getRelaxing());
+            textReligion.setText(patientProfile.getReligion());
+            textDance.setText(patientProfile.getDance());
         }
 
         btnMusic.setOnClickListener(this);
@@ -154,24 +187,61 @@ public class PatientActivity extends AppCompatActivity implements View.OnClickLi
         if(v==save)
         {
 
-//            String stFName=firstName.getText().toString();
-//            String stlName=lastName.getText().toString();
-//            String stAddress=address.getText().toString();
-//            String stFamilyPhone=familyPhone.getText().toString();
-//            String stYear=year.getText().toString();
-//            String stPId=pId.getText().toString();
+            String stFName=firstName.getText().toString();
+            String stlName=lastName.getText().toString();
+            String stAddress=address.getText().toString();
+            String stFamilyPhone=familyPhone.getText().toString();
+            String stYear=year.getText().toString();
+            String stPId=pId.getText().toString();
 
-//            patientDetails.setfName(stFName);
-//            patientDetails.setlName(stlName);
-//            patientDetails.setFamilyPhone(stFamilyPhone);
-//            patientDetails.setAddress(stAddress);
-//            patientDetails.setpId(stPId);
-//            patientDetails.setYear(stYear);
+            patientDetails.setfName(stFName);
+            patientDetails.setlName(stlName);
+            patientDetails.setFamilyPhone(stFamilyPhone);
+            patientDetails.setAddress(stAddress);
+            patientDetails.setpId(stPId);
+            patientDetails.setYear(stYear);
 
+            String stHomemusic=homemusic.getText().toString();
+            String stYoungmusic=youngmusic.getText().toString();
+            String stfavotitesinger=favoritesinger.getText().toString();
+            String stfavoritesong=favoritesong.getText().toString();
+            String stclassic=textClassic.getText().toString();
+            String stisrael=textIsrael.getText().toString();
+            String stlanguage=language.getText().toString();
+            String stDance=textDance.getText().toString();
+            String stloazi=textLoazi.getText().toString();
+            String stRelaxing=textRelaxing.getText().toString();
+            String stReligion=textReligion.getText().toString();
+            patientProfile.setHomeMusic(stHomemusic);
+            patientProfile.setYoungMusic(stYoungmusic);
+            patientProfile.setfSinger(stfavotitesinger);
+            patientProfile.setfSongs(stfavoritesong);
+            patientProfile.setClassic(stclassic);
+            patientProfile.setDance(stDance);
+            patientProfile.setIsrael(stisrael);
+            patientProfile.setLanguage(stlanguage);
+            patientProfile.setLoazi(stloazi);
+            patientProfile.setRelaxing(stRelaxing);
+            patientProfile.setReligion(stReligion);
+            ConnectToServer.connect(this);
+            try {
+                ConnectToServer.updatePatientDetails(patientDetails,(ProgressBar)findViewById(R.id.progressBar));
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            ConnectToServer.connect(this);
+            try {
+                ConnectToServer.updatePatientProfile(patientProfile,(ProgressBar)findViewById(R.id.progressBar));
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             Intent intent=new Intent(getBaseContext(),PatientListActivity.class);
             startActivity(intent);
-
-
         }
     }
     @Override
