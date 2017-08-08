497afb3f12c0ce909d7391a288310a0f52b31b8e
aleen <alona.nassra@hotmail.com>
2017-08-08 02:08:00 -0700

    end

--- app/src/main/java/com/example/singold/MyActivities/PlayerYouTubeActivity.java
+++ app/src/main/java/com/example/singold/MyActivities/PlayerYouTubeActivity.java
@@ -44,6 +44,7 @@ public class PlayerYouTubeActivity extends YouTubeBaseActivity implements YouTub
             }
         }
 
+
     @Override
     protected void onPause() {
         super.onPause();
