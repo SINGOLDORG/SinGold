7db86eade19dd5c8dea06e3f4d3864315cd3d590
aleen <alona.nassra@hotmail.com>
2017-08-08 13:14:23 -0700

    end

--- app/src/main/java/com/example/singold/data/SongAdapter.java
+++ app/src/main/java/com/example/singold/data/SongAdapter.java
@@ -51,8 +51,7 @@ public class SongAdapter extends ArrayAdapter<Song> {
             public void onClick(View view) {
                     Intent intent = new Intent(getContext(),
                             PlayerYouTubeActivity.class);
-//                    intent.putExtra("VIDEO_ID",song.getLink());
-                    intent.putExtra("VIDEO_ID",song.getName());
+                    intent.putExtra("VIDEO_ID",song.getLink());
                 getContext().startActivity(intent);
             }
         });
